2f674bda9b114219655fb4a942607c686bf33e98
RYANFRANKLIN237 <Ngometune@gmail.com>
2024-05-10 10:32:27 +0100

    Implementing media player

--- app/src/main/java/com/example/musicapp/adapters/SongsListAdapter.kt
+++ app/src/main/java/com/example/musicapp/adapters/SongsListAdapter.kt
@@ -1,16 +1,17 @@
 package com.example.musicapp.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.example.musicapp.SongsListActivity
+import com.example.musicapp.MyExoplayer
+import com.example.musicapp.PlayerActivity
 import com.example.musicapp.databinding.SongListItemRecyclerRowBinding
 import com.example.musicapp.models.SongModel
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.toObject
 
 class SongsListAdapter(private val songIdList: List<String>) :
     RecyclerView.Adapter<SongsListAdapter.MyViewHolder>() {
@@ -20,7 +21,7 @@ class SongsListAdapter(private val songIdList: List<String>) :
         fun bindData(songId: String){
             FirebaseFirestore.getInstance().collection("songs")
                 .document(songId).get()
-                .addOnSuccessListener {
+                .addOnSuccessListener { it ->
                     val song = it.toObject(SongModel::class.java)
                     song?.apply {
                         binding.songTitleTextView.text = title
@@ -30,6 +31,11 @@ class SongsListAdapter(private val songIdList: List<String>) :
                                 RequestOptions().transform(RoundedCorners(32))
                             )
                             .into(binding.songCoverImageView)
+                        binding.root.setOnClickListener {
+                            MyExoplayer.startPlaying(binding.root.context,song)
+                            //navigate to player activity
+                            it.context.startActivity(Intent(it.context,PlayerActivity::class.java))
+                        }
                     }
                 }
         }
